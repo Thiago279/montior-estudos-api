1548db40dc2e136e56183ff5b051d647853bbf66
Thiago Toma <thiagotoma99@gmail.com>
2026-08-10 12:40:15 -0300

    feat(config): configure CORS to allow requests from frontend

--- src/main/java/com/toma/monitor_estudos/controller/EstatisticasController.java
+++ src/main/java/com/toma/monitor_estudos/controller/EstatisticasController.java
@@ -13,10 +13,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
